9b30c2d4f7556d04764cb9dff7ca7c8b52b7c5f3
Daniel Wu <111389053+Chih-Hao-Wu@users.noreply.github.com>
2022-12-26 16:55:24 -0500

    Update check_progress.sh

--- check_progress.sh
+++ check_progress.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# get sample names depending on whether the variants have been callled and then annotated
 ls calls/mutect2/*/*.vcf.gz | grep -v "chr" | cut -d '/' -f3 > .progress/mutect2_calls.txt
 ls -l filter/filtered_case-*.gz | awk -F 'filtered_' '{print $2}' | cut -d '.' -f1 > .progress/annotated_calls.txt
 
+# get counts
 num_complete=$(wc -l < .progress/mutect2_calls.txt)
 num_annotated=$(wc -l < .progress/annotated_calls.txt)
 
+# print counts to line
 printf "Number samples with completed calls: $num_complete \n"
 printf "Fraction samples with annotated calls: $num_annotated/$num_complete \n"
